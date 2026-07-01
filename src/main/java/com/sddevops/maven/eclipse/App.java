65e90b9c6e6122d59aa0f8d8148d46ae23cadf2e
erdawati88 <erdawati@gmail.com>
2026-07-02 03:11:39 +0800

    feat: Add introductory line to test commit and push feature

--- src/main/java/com/sddevops/maven/eclipse/App.java
+++ src/main/java/com/sddevops/maven/eclipse/App.java
@@ -1,11 +1,12 @@
 package com.sddevops.maven.eclipse;
 
 /**
- * Hello world!
+ * Hello Again
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Erdawati");
         System.out.println("new line");
+        System.out.println("first change");
     }
 }
